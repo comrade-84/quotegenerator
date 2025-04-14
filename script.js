4f1453fd27b82131e401e1d2a4d2ed39bb44037a
saliu ridwan <ridwansaliu84@gmail.com>
2025-04-14 11:04:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const quoteId = document.getElementById('idnumber');
+const quoteMessage = document.getElementById('quote');
+const generateBtn = document.querySelector('.image');
+let advice ;
+async function getQuote() {
+   try {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    advice = await response.json();
+    console.log(advice);
+   
+    quoteId.textContent = advice.slip.id;
+    quoteMessage.textContent = `"${advice.slip.advice}"`;
+   } catch (error) {
+    console.log('error fetching quote:', error);
+   } 
+}
+generateBtn.addEventListener('click', function() {
+    getQuote();
+});
+setInterval(getQuote,10000)
